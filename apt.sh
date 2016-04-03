90b38134839881c13632c59615e61baa473439d4
xuehao zhou <robertzhouxh@gmail.com>
2016-04-03 13:50:08 +0800

    add -y flag for apt-get install in apt

--- apt.sh
+++ apt.sh
@@ -85,7 +85,7 @@ select yn in "Yes" "No"; do
 	case $yn in
 		Yes )
 		    cecho "Ok! installing apps, please wait ... " $yellow
-		    sudo apt-get install ${apps[@]}
+		    sudo apt-get install -y ${apps[@]}
 		    break;;
 		No ) break;;
 	esac
